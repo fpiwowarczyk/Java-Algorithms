b5f1af1063ef3286f1b6812f2148b00dd66cf393
fpiwowarczyk <filip.piwowarczyk1997@gmail.com>
2020-09-02 16:33:24 +0200

    Add queue data struct

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
         //test.testSinglyLinkedList();
         //test.testDoublyLinkedList();
+        test.testQueue();
     }
 
 
--- src/test/Test.java
+++ src/test/Test.java
@@ -2,6 +2,7 @@ package test;
 
 import List.DoublyLinkedList;
 import List.SinglyLinkedList;
+import Queue.Queue;
 
 public class Test {
 
@@ -49,5 +50,33 @@ public class Test {
         myList.insertOrdered(12);
         myList.insertOrdered(10);
         myList.display();
+    }
+
+    public void testQueue(){
+        Queue myQueue = new Queue(4);
+
+        myQueue.insert(10);
+        myQueue.insert(4);
+        myQueue.insert(1);
+        myQueue.insert(99);
+        // [10(first to remove),4,1,99(last to remove)]
+        System.out.println(myQueue.isFull()); // True
+
+        System.out.println(myQueue.peekFront()); // 10 Should be front
+        System.out.println(myQueue.remove()); // Print 10 and remove 10
+        System.out.println(myQueue.peekFront()); // Now 4 will be front
+        System.out.println(myQueue.peekBack()); // 99 Should be back
+        myQueue.insert(13); //new back is 13
+        System.out.println(myQueue.peekBack()); // Print 13
+
+        System.out.println(myQueue.isEmpty()); // False isn't empty
+        myQueue.remove();
+        myQueue.remove();
+        myQueue.remove();
+        myQueue.remove();
+        System.out.println(myQueue.isEmpty()); // True is empty
+
+
+
     }
 }
